9235d8a93426bc4968eb2aae2647db5cf25e707d
Mardc89 <mardc0602@gmail.com>
2025-03-13 14:10:50 -0500

    Agregando ProductoService

---
+++ APIRestPedidos/SistemaPedidos.BLL/Servicios/CategoriaService.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using SistemaPedidos.BLL.Servicios.Contrato;
+using SistemaPedidos.DAL.Repositorios.Contrato;
+using SistemaPedidos.DTO;
+using SistemaPedidos.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPedidos.BLL.Servicios
+{
+    public class CategoriaService:ICategoriaService
+    {
+        private readonly IGenericRepositorio<Categoria> _categoriaRepositorio;
+        private readonly IMapper _mapper;
+
+        public CategoriaService(IGenericRepositorio<Categoria> categoriaRepositorio, IMapper mapper)
+        {
+            _categoriaRepositorio =categoriaRepositorio;
+            _mapper=mapper;
+        }
+
+        public async Task<List<CategoriaDTO>> Lista()
+        {
+            try
+            {
+                var listaCategorias =await  _categoriaRepositorio.Consultar();
+                return _mapper.Map<List<CategoriaDTO>>(listaCategorias.ToList());
+            }
+            catch 
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ APIRestPedidos/SistemaPedidos.BLL/Servicios/ProductoService.cs
@@ -0,0 +1,59 @@
+﻿using AutoMapper;
+using SistemaPedidos.BLL.Servicios.Contrato;
+using SistemaPedidos.DAL.Repositorios.Contrato;
+using SistemaPedidos.DTO;
+using SistemaPedidos.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaPedidos.BLL.Servicios
+{
+    public class ProductoService:IProductoService
+    {
+        private readonly IGenericRepositorio<Producto> _productoRepositorio;
+        private IMapper _mapper;
+
+        public ProductoService(IGenericRepositorio<Producto> productoRepositorio, IMapper mapper)
+        {
+            _productoRepositorio = productoRepositorio;
+            _mapper = mapper;
+                
+        }
+
+        public async Task<ProductoDTO> Crear(ProductoDTO modelo)
+        {
+                
+            try                                  
+            {
+                var productoCreado = await _productoRepositorio.Crear(_mapper.Map<Producto>(modelo));
+
+                if (productoCreado.IdProducto == 0)
+                    throw new TaskCanceledException("No se pudo Crear");
+
+                return _mapper.Map<ProductoDTO>(productoCreado);
+            }
+            catch 
+            {
+                throw;
+            }
+        }
+
+        public Task<bool> Editar(ProductoDTO modelo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Eliminar(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<ProductoDTO>> Lista()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
